6946953457ebbcffc2471424777d0654b5d57308
teekaytech <taofeek.olalere@feghas.com>
2020-11-27 19:33:36 +0100

    Initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  skip_before_action :require_login, only: [:create, :index]
+
+  def index
+    render json: {
+      response: 'Welcome.'
+    }
+  end
+
+  def create
+    user = User.create(user_params)
+    if user.valid?
+        payload = {user_id: user.id}
+        token = encode_token(payload)
+        puts token
+        render json: {user: user, jwt: token}
+    else
+        render json: {errors: user.errors.full_messages}, status: :not_acceptable
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resource :users, only: [:create]
+  post "/login", to: "auth#login"
+  get "/auto_login", to: "auth#auto_login"
+  get "/user_is_authed", to: "auth#user_is_authed"
+  root to: 'users#index'
+end
